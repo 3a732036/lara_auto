35317af42deacd22f8d306b3e79d9d824476029f
Jun-You Ye <3a732036@gm.student.ncut.edu.tw>
2020-10-14 12:43:22 +0800

    日誌檔(your.log)改存放於專案的log資料夾

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ use Monolog\Handler\StreamHandler;
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler('lara_auto/log/your.log', Logger::WARNING));
+$log->pushHandler(new StreamHandler('../log/your.log', Logger::WARNING));
 
 // add records to the log
 $log->warning('Foo');
